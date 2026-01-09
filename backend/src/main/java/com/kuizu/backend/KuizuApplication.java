f5b535a9993dfc17b9bf73ee525fb0d5b743e97d
minhkhoin3913 <nguyenminhkhoi3913@gmail.com>
2026-01-09 12:50:52 +0700

    Initialize project with React and Spring Boot setup

---
+++ backend/src/main/java/com/kuizu/backend/KuizuApplication.java
@@ -0,0 +1,13 @@
+package com.kuizu.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuizuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuizuApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/kuizu/backend/KuizuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuizu.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuizuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
